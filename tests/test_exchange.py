cb7cc0840c69ae2692042208de28f5eff3d5858c
Michael Egorov <michwill@gmail.com>
2019-09-23 02:49:20 -0700

    Run simulations without verifying

--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -1,4 +1,7 @@
 import time
+import random
+from itertools import permutations
+from simulation import Curve
 
 U = 10 ** 18
 N_COINS = 3
@@ -57,6 +60,9 @@ def test_simulated_exchange(w3, coins, swap):
     swap.add_liquidity(0, 100 * U, 110 * U, int(time.time()) + 3600,
                        transact={'from': sam})
 
+    # Model
+    curve = Curve(2 * 360, N_COINS * 100 * U, N_COINS)
+
     for c in coins:
         # Fund the customer with $100 of each coin
         c.transfer(bob, 100 * U,
@@ -64,4 +70,12 @@ def test_simulated_exchange(w3, coins, swap):
         # Approve by Bob
         c.approve(swap.address, 100 * U, transact={'from': bob})
 
-    # The scene is now set up
+    # Start trading!
+    for k in range(50):
+        i, j = random.choice(list(permutations(range(N_COINS), 2)))
+        value = random.randrange(2 * U)
+        swap.exchange(i, j, value,
+                      int(0.5 * value), int(time.time()) + 3600,
+                      transact={'from': bob})
+        curve.exchange(i, j, value)
+        # TODO: test outputs
