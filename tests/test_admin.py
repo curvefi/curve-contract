1e825847d27780c1766067b57d715a82e6a3af71
Michael Egorov <michwill@gmail.com>
2019-09-29 18:36:02 -0700

    Test admin earning admin fees

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,6 +1,11 @@
 import pytest
+import random
+from itertools import permutations
 from time import time
 from eth_tester.exceptions import TransactionFailed
+
+U = 10 ** 18
+N_COINS = 3
 
 
 def test_transfer_ownership(tester, w3, swap):
@@ -46,3 +51,48 @@ def test_transfer_ownership(tester, w3, swap):
     # Cannot transfer after reverting
     with pytest.raises(TransactionFailed):
         swap.functions.apply_transfer_ownership().transact({'from': alice})
+
+
+def test_trade_and_withdraw_fees(tester, w3, coins, swap):
+    alice, owner, bob = w3.eth.accounts[:3]
+    # Owner is the exchange admin
+    # Alice and Bob trade
+
+    # Owner wants to charge 20%
+    swap.functions.commit_new_parameters(
+        2 * 360, int(0.001 * 10 ** 10), int(0.2 * 10 ** 10)
+        ).transact({'from': owner})
+
+    for c in coins:
+        c.functions.transfer(bob, 1000 * U).transact({'from': alice})
+        c.functions.approve(swap.address, 1000 * U).transact({'from': alice})
+
+    swap.functions.add_liquidity([1000 * U] * N_COINS, int(time()) + 3600).\
+        transact({'from': alice})
+
+    current_time = int(time()) + 86400 * 7 + 2000
+    tester.time_travel(current_time)
+    tester.mine_block()
+
+    swap.functions.apply_new_parameters().transact({'from': owner})
+
+    volumes = [0, 0, 0]
+
+    for k in range(10):
+        i, j = random.choice(list(permutations(range(N_COINS), 2)))
+        value = random.randrange(100 * U)
+        y_0 = coins[j].caller.balanceOf(bob)
+        coins[i].functions.approve(swap.address, value).transact({'from': bob})
+        swap.functions.exchange(i, j, value,
+                                int(0.5 * value), current_time + 3600).\
+            transact({'from': bob})
+        y_1 = coins[j].caller.balanceOf(bob)
+
+        volumes[j] += y_1 - y_0
+
+    swap.functions.withdraw_admin_fees().transact({'from': owner})
+
+    for v, c in zip(volumes, coins):
+        b = c.caller.balanceOf(owner)
+        f = int((v / 0.999 - v) * 0.2)
+        assert abs(1 - b / f) < 2e-10 * 0.001
