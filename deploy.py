49ca3d3100090d836b7a6deb3c4ae020b0cfc1ed
Michael Egorov <michwill@gmail.com>
2019-10-01 00:29:08 -0700

    Deployer works

--- deploy.py
+++ deploy.py
@@ -3,21 +3,37 @@
 # from os.path import expanduser
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
-from tests import deploy_contract
+from tests.deploy import deploy_contract
 
 # Deployment parameters
 # PROVIDER_URI = expanduser('~/.ethereum/testnet/geth.ipc')
 PROVIDER_URI = '/tmp/geth.ipc'
 POA = True
+FUND_DEV = True
 N_COINS = 3
 SWAP_DEPLOY_ADDRESS = '0xFD3DeCC0cF498bb9f54786cb65800599De505706'
 COINS_DEPLOY_ADDRESS = '0x9CA6Eebc54efF56F1D0e844a41364c8aF2321520'
+
+HELP = """coins = deploy_test_erc20() to deploy test coins
+swap, token = deploy_swap(coins, A, fee) to deploy swap contract from the list
+====================================================="""
 
 
 provider = Web3.IPCProvider(PROVIDER_URI)
 w3 = Web3(provider)
 if POA:
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+if FUND_DEV:
+    txhash = w3.eth.sendTransaction({
+        'from': w3.eth.accounts[0],
+        'to': SWAP_DEPLOY_ADDRESS,
+        'value': 10 ** 19})
+    w3.eth.waitForTransactionReceipt(txhash)
+    txhash = w3.eth.sendTransaction({
+        'from': w3.eth.accounts[0],
+        'to': COINS_DEPLOY_ADDRESS,
+        'value': 10 ** 19})
+    w3.eth.waitForTransactionReceipt(txhash)
 
 
 def deploy_test_erc20():
@@ -36,13 +52,12 @@ def deploy_swap(coins, A, fee):
     swap_contract = deploy_contract(
             w3, ['stableswap.vy', 'ERC20m.vy'], SWAP_DEPLOY_ADDRESS,
             coins, pool_token.address, A, fee)
-    pool_token.functions.set_minter(swap_contract.address).transact(
+    txhash = pool_token.functions.set_minter(swap_contract.address).transact(
         {'from': SWAP_DEPLOY_ADDRESS})
+    w3.eth.waitForTransactionReceipt(txhash)
     return swap_contract.address, pool_token.address
 
 
 if __name__ == '__main__':
-    print('coins = deploy_test_erc20() to deploy test coins')
-    print('swap, token = deploy_swap(coins, A, fee) to deploy swap contract from the list')
     import IPython
-    IPython.embed()
+    IPython.embed(header=HELP)
