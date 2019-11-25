670e91087f14bf4bdbcb5bbb61e7fcea1dabe367
Michael Egorov <michwill@gmail.com>
2019-11-25 21:17:10 +0100

    test_curve works

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -25,5 +25,5 @@ def test_curve_in_contract(w3, coins, swap, n_coins):
         for i, j in permutations(range(3), 2):
             dx = random.randrange(2 * n_coins * UU[i])
             dy_1 = swap.caller.get_dy(i, j, dx)
-            dy_2 = curve.dy(i, j, dx)
+            dy_2 = curve.dy(i, j, dx * max(UU) // UU[i]) * UU[j] // max(UU)
             assert dy_1 == dy_2
