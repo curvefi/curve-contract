b1f1cbba7bce4a13ce4c021f826211c6e1202f2d
Michael Egorov <michwill@gmail.com>
2020-04-17 17:22:33 -0700

    Check that we deposited correct amount

--- tests/test_zap.py
+++ tests/test_zap.py
@@ -1,5 +1,7 @@
 import pytest
 from random import randrange
+from eth_tester.exceptions import TransactionFailed
+from .conftest import UU, UUY
 
 
 @pytest.mark.parametrize("amounts", [[randrange(1000, 30000) for i in range(5)] for j in range(4)])
@@ -10,6 +12,8 @@ def test_deposit_withdraw(w3, ypool, coins, coins_y, yerc20s, yerc20s_y, pool_to
     coins = [coins[0]] + coins_y
     ycoins = [yerc20s[0]] + yerc20s_y
 
+    U = [UU[0]] + UUY
+    amounts = [u * a for u, a in zip(U, amounts)]
     rates = [c.caller.getPricePerFullShare() for c in ycoins]
     camounts = [a * 10 ** 18 // r for a, r in zip(amounts, rates)]
     half_amounts = [a // 2 for a in amounts]
@@ -19,7 +23,13 @@ def test_deposit_withdraw(w3, ypool, coins, coins_y, yerc20s, yerc20s_y, pool_to
         c.functions.approve(deposit.address, amount).transact(from_sam)
     # For the first deposit, calc_token_amount is not working
     deposit.functions.add_liquidity(half_amounts, 0).transact(from_sam)
-    return  # XXX
     # The second one should though
     token_amount = deposit.caller.calc_token_amount(camounts, True)
-    deposit.functions.add_liquidity(half_amounts, int(1.01 * token_amount)).transact(from_sam)
+    with pytest.raises(TransactionFailed):
+        deposit.functions.add_liquidity(half_amounts, int(1.01 * token_amount / 2)).transact(from_sam)
+    deposit.functions.add_liquidity(half_amounts, int(0.99 * token_amount / 2)).transact(from_sam)
+
+    # Test that we have amounts deposited
+    assert abs(swap2.caller.balances(0) - camounts[0]) <= 1
+    for i in range(1, 5):
+        assert abs(ypool.caller.balances(i - 1) - camounts[i]) <= 1
