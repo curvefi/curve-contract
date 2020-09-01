896603fb99e18d810ac2ca8bab5a5fc49f254f17
Michael Egorov <michwill@gmail.com>
2020-09-01 18:36:19 +0300

    test_exchange_reverts

--- tests/unitary/test_exchange_reverts.py
+++ tests/unitary/test_exchange_reverts.py
@@ -9,28 +9,36 @@ INITIAL_AMOUNTS = [10**(i+6) for i in PRECISIONS]
 
 
 @pytest.fixture(scope="module", autouse=True)
-def setup(alice, bob, coins, swap):
-    for coin, amount in zip(coins, INITIAL_AMOUNTS):
+def setup(alice, bob, bank, coins, swap):
+    for coin, amount in zip(coins[:-1], INITIAL_AMOUNTS[:-1]):
         coin._mint_for_testing(alice, amount, {'from': alice})
         coin.approve(swap, 2**256-1, {'from': alice})
         coin.approve(swap, 2**256-1, {'from': bob})
+        coin._mint_for_testing(bank, amount, {'from': bank})
+
+    # Leave Alice 1 million of metacoins
+    coins[-1].transfer(
+            bank, coins[-1].balanceOf(alice) - INITIAL_AMOUNTS[-1],
+            {'from': alice})
+    coins[-1].approve(swap, 2**256-1, {'from': alice})
+    coins[-1].approve(swap, 2**256-1, {'from': bob})
 
     swap.add_liquidity(INITIAL_AMOUNTS, 0, {'from': alice})
 
 
 @pytest.mark.parametrize("sending,receiving", itertools.permutations([0, 1], 2))
-def test_insufficient_balance(bob, swap, coins, sending, receiving):
+def test_insufficient_balance(bob, bank, swap, coins, sending, receiving):
     amount = 10**PRECISIONS[sending]
-    coins[sending]._mint_for_testing(bob, amount, {'from': bob})
+    coins[sending].transfer(bob, amount, {'from': bank})
 
     with brownie.reverts():
         swap.exchange(sending, receiving, amount+1, 0, {'from': bob})
 
 
 @pytest.mark.parametrize("sending,receiving", itertools.permutations([0, 1], 2))
-def test_min_dy_too_high(bob, swap, coins, sending, receiving):
+def test_min_dy_too_high(bob, bank, swap, coins, sending, receiving):
     amount = 10**PRECISIONS[sending]
-    coins[sending]._mint_for_testing(bob, amount, {'from': bob})
+    coins[sending].transfer(bob, amount, {'from': bank})
 
     min_dy = swap.get_dy(sending, receiving, amount)
     with brownie.reverts("Exchange resulted in fewer coins than expected"):
