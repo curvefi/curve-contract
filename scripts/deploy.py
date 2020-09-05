194886067572d6c59ffdb1601313fcfdd64f95b4
Michael Egorov <michwill@gmail.com>
2020-09-06 01:32:50 +0200

    Deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,14 +7,15 @@ from brownie import CurveToken, LiquidityGauge, StableSwap, accounts, web3
 
 POOL_OWNER = "0x6e8f6D1DA6232d5E40b0B8758A0145D6C5123eB7"  # PoolProxy
 MINTER = "0xd061D61a4d941c39E5453435B6345Dc261C2fcE0"
-HBTC = "0x0316EB71485b0Ab14103307bf65a021042c6d380"
-WBTC = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 
-AMP = 100
+AMP = 150
 FEE = 4000000
 ADMIN_FEE = 0
 
-REQUIRED_CONFIRMATIONS = 1
+REQUIRED_CONFIRMATIONS = 3
 
 
 def main(confs=REQUIRED_CONFIRMATIONS, apply_strategies=True):
@@ -28,8 +29,8 @@ def main(confs=REQUIRED_CONFIRMATIONS, apply_strategies=True):
         web3.middleware_onion.add(middleware.simple_cache_middleware)
 
     token = CurveToken.deploy(
-        "Curve.fi hBTC/wBTC",
-        "hCRV",
+        "Curve.fi DAI/USDC/USDT",
+        "3Crv",
         18,
         0,
         {'from': deployer, 'required_confs': confs}
@@ -37,7 +38,7 @@ def main(confs=REQUIRED_CONFIRMATIONS, apply_strategies=True):
 
     swap = StableSwap.deploy(
         POOL_OWNER,
-        [HBTC, WBTC],
+        [DAI, USDC, USDT],
         token,
         AMP,
         FEE,
