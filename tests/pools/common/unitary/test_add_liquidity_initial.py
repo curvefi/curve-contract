ff985472c81d11e1c080e7ff45228c2340b5174a
Ben Hauser <ben@hauser.id>
2020-09-16 15:49:51 +0300

    test: move tests/common to tests/pools/common

--- tests/pools/common/unitary/test_add_liquidity_initial.py
+++ tests/pools/common/unitary/test_add_liquidity_initial.py
@@ -1,13 +1,7 @@
 import brownie
 import pytest
 
-
-@pytest.fixture(scope="module", autouse=True)
-def setup(alice, wrapped_coins, swap):
-    for coin in wrapped_coins:
-        coin._mint_for_testing(alice, 10**24, {'from': alice})
-        coin.approve(swap, 10**24, {'from': alice})
-
+pytestmark = pytest.mark.usefixtures("add_initial_liquidity", "mint_bob", "approve_bob")
 
 @pytest.mark.parametrize("min_amount", [0, 2 * 10**18])
 def test_initial(alice, swap, wrapped_coins, pool_token, min_amount, wrapped_decimals, n_coins):
