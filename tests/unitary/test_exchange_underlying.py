fc49e5543b32818b639b56e4aa4d8d51b54fb25b
Michael Egorov <michwill@gmail.com>
2020-09-16 13:40:48 +0200

    test_exchange_underlying()

--- tests/unitary/test_exchange_underlying.py
+++ tests/unitary/test_exchange_underlying.py
@@ -2,14 +2,15 @@ import itertools
 
 import pytest
 
-from tests.conftest import PRECISIONS, BASE_PRECISIONS
+from tests.conftest import PRECISIONS, BASE_PRECISIONS, N_COINS
 
 INITIAL_AMOUNTS = [10**(i+6) for i in PRECISIONS]
+BASE_INITIAL_AMOUNTS = [10**(i+6) for i in BASE_PRECISIONS]
 
 
 @pytest.fixture(scope="module", autouse=True)
-def setup(alice, bob, bank, coins, swap):
-    for coin, amount in zip(coins[:-1], INITIAL_AMOUNTS[:-1]):
+def setup(alice, bob, bank, coins, base_coins, swap):
+    for coin, amount in zip(coins[:-1] + base_coins, INITIAL_AMOUNTS[:-1] + BASE_INITIAL_AMOUNTS):
         coin._mint_for_testing(alice, amount, {'from': alice})
         coin.approve(swap, 2**256-1, {'from': alice})
         coin.approve(swap, 2**256-1, {'from': bob})
@@ -35,6 +36,10 @@ def test_exchange_underlying(chain, alice, bob, bank, swap, coins, sending, rece
     underlying_coins = coins[:-1] + base_coins
     underlying_precisions = PRECISIONS[:-1] + BASE_PRECISIONS
     base_fee = base_swap.fee() / 1e10
+    if (sending >= len(coins)-1 and receiving < len(coins)-1) or (sending < len(coins)-1 and receiving >= len(coins)-1):
+        base_fee /= 2
+    elif sending < len(coins)-1 and receiving < len(coins)-1:
+        base_fee = 0
 
     if base_volume:
         base_coins[0]._mint_for_testing(alice, base_volume * 10**BASE_PRECISIONS[0], {'from': alice})
@@ -48,6 +53,9 @@ def test_exchange_underlying(chain, alice, bob, bank, swap, coins, sending, rece
         chain.sleep(86400*3)
         swap.apply_new_fee({'from': alice})
 
+    if sending >= len(coins)-1 and receiving >= len(coins)-1:
+        fee = 0
+
     amount = 10**underlying_precisions[sending]
     underlying_coins[sending].transfer(bob, amount, {'from': bank})
     swap.exchange_underlying(sending, receiving, amount, 0, {'from': bob})
@@ -55,15 +63,20 @@ def test_exchange_underlying(chain, alice, bob, bank, swap, coins, sending, rece
     assert underlying_coins[sending].balanceOf(bob) == 0
 
     received = underlying_coins[receiving].balanceOf(bob)
-    assert (0.9999 - fee - base_fee/2) < received / 10**underlying_precisions[receiving] < (1.0001-fee-base_fee/2)
+    assert (0.9999 - fee - base_fee) <= received / 10**underlying_precisions[receiving] <= (1.0001-fee-base_fee)  # XXX
 
-    # XXX fees are not in underlying
-    expected_admin_fee = 10**underlying_precisions[receiving] * fee * admin_fee
+    virtual_price = base_swap.get_virtual_price() / 1e18
+    base_receiving = min(receiving, N_COINS-1)
+    if receiving >= N_COINS - 1 and sending < N_COINS - 1:
+        expected_admin_fee = 10**PRECISIONS[base_receiving] * fee * admin_fee / virtual_price
+    if sending >= N_COINS - 1 and receiving < N_COINS - 1:
+        expected_admin_fee = 10**PRECISIONS[base_receiving] * fee * admin_fee * virtual_price
+    expected_admin_fee = 10**PRECISIONS[base_receiving] * fee * admin_fee
 
     if expected_admin_fee:
-        assert (expected_admin_fee * 0.998) <= swap.admin_balances(receiving) <= (1.02 * expected_admin_fee)
+        assert (expected_admin_fee * 0.998) <= swap.admin_balances(base_receiving) <= (1.02 * expected_admin_fee)
     else:
-        assert swap.admin_balances(receiving) == 0
+        assert swap.admin_balances(base_receiving) == 0
 
 
 @pytest.mark.parametrize("sending,receiving", itertools.permutations(range(4), 2))
