62e1cfc7cfec06f8e13ddb9d332951deea87263f
Ben Hauser <ben@hauser.id>
2020-09-27 04:09:02 +0300

    chore: expand brownie_hooks to support meta pools

--- brownie_hooks.py
+++ brownie_hooks.py
@@ -8,24 +8,51 @@ This file should not be modified directly. Values are set based on the
 import json
 
 
+def _load_pool_data(path):
+    with path.parent.joinpath('pooldata.json').open() as fp:
+        data = json.load(fp)
+
+    decimals = [i['decimals'] for i in data['coins']]
+    precision_multiplier = [10**18 // (10**i) for i in decimals]
+
+    return {
+        'n_coins': len(decimals),
+        'decimals': decimals,
+        'precision_mul': precision_multiplier,
+        'rates': [i*10**18 for i in precision_multiplier],
+        'lending': [i['wrapped'] for i in data['coins']],
+        'base_pool_contract': data.get("base_pool_contract"),
+    }
+
+
 def brownie_load_source(path, source):
 
     if "pool-templates" not in path.parts:
+        # compile-time substitution only applies to pool templates
         return source
 
-    with path.parent.joinpath('pooldata.json').open() as fp:
-        pool_data = json.load(fp)
-
-    decimals = [i['decimals'] for i in pool_data['coins']]
-    precision_multiplier = [10**18 // (10**i) for i in decimals]
-    rates = [i*10**18 for i in precision_multiplier]
+    data_path = path.parent.joinpath('pooldata.json')
+    pool_data = _load_pool_data(data_path)
 
     replacements = {
-        '___N_COINS___': len(decimals),
-        '___PRECISION_MUL___': precision_multiplier,
-        '___RATES___': rates,
-        '___USE_LENDING___': [i['wrapped'] for i in pool_data['coins']],
+        '___N_COINS___': pool_data['n_coins'],
+        '___PRECISION_MUL___': pool_data['precision_mul'],
+        '___RATES___': pool_data['rates'],
+        '___USE_LENDING___': pool_data['lending'],
     }
+
+    if pool_data["base_pool_contract"]:
+        # for metapools, also load pool data for the base pool
+        contracts_dir = next(i for i in path.parents if i.name == "contracts")
+        swap_dir = next(contracts_dir.glob(f"**/{pool_data['base_pool_contract']}.vy")).parent
+        data_path = swap_dir.joinpath("pooldata.json")
+        pool_data = _load_pool_data(data_path)
+
+        replacements.update({
+            '___BASE_N_COINS___': pool_data['n_coins'],
+            '___BASE_PRECISION_MUL___': pool_data['precision_mul'],
+            '___BASE_RATES___': pool_data['rates'],
+        })
 
     for k, v in replacements.items():
         source = source.replace(k, str(v))
