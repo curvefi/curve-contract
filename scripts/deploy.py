9f515444aea1ee31eea004d340428fd8b59c29ff
Ben Hauser <ben@hauser.id>
2020-10-03 00:22:33 +0300

    feat: generalize deployment script to pull data from pooldata.json

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,39 +1,21 @@
 import json
 import requests
 
-from brownie import CurveTokenV2, StableSwapBase, StableSwapYLend, DepositYLend, accounts
+from brownie import accounts
+from brownie.project.main import get_loaded_projects
 
 # modify this import if you wish to deploy a different liquidity gauge
 from brownie import LiquidityGauge as LiquidityGauge
 
 # set a throwaway admin account here
 DEPLOYER = accounts.add()
-
 REQUIRED_CONFIRMATIONS = 1
 
 # deployment settings
-LP_TOKEN_NAME = "Curve.fi xx/xx/xx"
-LP_TOKEN_SYMBOL = "[x]CRV"
-WRAPPED_COINS = []
-UNDERLYING_COINS = [
-    # "0x6b175474e89094c44da98b954eedeac495271d0f",  # DAI
-    # "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",  # USDC
-    # "0xdac17f958d2ee523a2206206994597c13d831ec7",  # USDT
-]
-AMP = 100
-FEE = 4000000           # 0.04%
-ADMIN_FEE = 5000000000  # 50% of FEE
-
+# most settings are taken from `contracts/pools/{POOL_NAME}/pooldata.json`
+POOL_NAME = ""
 POOL_OWNER = "0x6e8f6D1DA6232d5E40b0B8758A0145D6C5123eB7"  # PoolProxy
 MINTER = "0xd061D61a4d941c39E5453435B6345Dc261C2fcE0"
-
-
-def base(confs=REQUIRED_CONFIRMATIONS):
-    _deploy(confs, None, None)
-
-
-def ylend(confs=REQUIRED_CONFIRMATIONS):
-    _deploy(confs, StableSwapYLend, DepositYLend)
 
 
 def _gas_price():
@@ -42,56 +24,68 @@ def _gas_price():
     return data['data']['fast'] + 10**9
 
 
-def _deploy(confs, lending_contract, deposit_contract):
+def _tx_params():
+    return {'from': DEPLOYER, 'required_confs': REQUIRED_CONFIRMATIONS, 'gas_price': _gas_price()}
 
-    token = CurveTokenV2.deploy(
-        LP_TOKEN_NAME,
-        LP_TOKEN_SYMBOL,
-        18,
-        0,
-        {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
+
+def main():
+    project = get_loaded_projects()[0]
+
+    # load data about the deployment from `pooldata.json`
+    contracts_path = project._path.joinpath("contracts/pools")
+    with contracts_path.joinpath(f"{POOL_NAME}/pooldata.json").open() as fp:
+        pool_data = json.load(fp)
+
+    swap_name = next(i.stem for i in contracts_path.glob(f"{POOL_NAME}/StableSwap*"))
+    swap_deployer = getattr(project, swap_name)
+    token_deployer = getattr(project, pool_data.get('lp_contract'))
+
+    underlying_coins = [i['underlying_address'] for i in pool_data['coins']]
+    wrapped_coins = [i.get('wrapped_address', i['underlying_address']) for i in pool_data['coins']]
+
+    base_pool = None
+    if "base_pool" in pool_data:
+        with contracts_path.joinpath(f"{pool_data['base_pool']}/pooldata.json").open() as fp:
+            base_pool_data = json.load(fp)
+            base_pool = base_pool_data['swap_address']
+
+    # deploy the token
+    token_args = pool_data["lp_constructor"]
+    token = token_deployer.deploy(token_args['name'], token_args['symbol'], 18, 0, _tx_params())
+
+    # deploy the pool
+    abi = next(i['inputs'] for i in swap_deployer.abi if i['type'] == "constructor")
+    args = pool_data["swap_constructor"]
+    args.update(
+        _coins=wrapped_coins,
+        _underlying_coins=underlying_coins,
+        _pool_token=token,
+        _base_pool=base_pool,
+        _owner=POOL_OWNER,
     )
+    deployment_args = [args[i['name']] for i in abi] + [_tx_params()]
 
-    if lending_contract is not None:
-        swap = lending_contract.deploy(
-            POOL_OWNER,
-            WRAPPED_COINS,
-            UNDERLYING_COINS,
-            token,
-            AMP,
-            FEE,
-            ADMIN_FEE,
-            {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
-        )
-        deposit_contract.deploy(
-            WRAPPED_COINS,
-            UNDERLYING_COINS,
-            swap,
-            token,
-            {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
-        )
+    swap = swap_deployer.deploy(*deployment_args)
 
-    else:
-        swap = StableSwapBase.deploy(
-            POOL_OWNER,
-            UNDERLYING_COINS,
-            token,
-            AMP,
-            FEE,
-            ADMIN_FEE,
-            {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
-        )
+    # set the minter
+    token.set_minter(swap, _tx_params())
 
-    with open('StableSwap.abi', 'w') as fp:
-        json.dump(swap.abi, fp, indent=True)
+    # deploy the liquidity gauge
+    LiquidityGauge.deploy(token, MINTER, _tx_params())
 
-    token.set_minter(
-        swap,
-        {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
-    )
+    # deploy the zap
+    zap_name = next((i.stem for i in contracts_path.glob(f"{POOL_NAME}/Deposit*")), None)
+    if zap_name is not None:
+        zap_deployer = getattr(project, zap_name)
 
-    LiquidityGauge.deploy(
-        token,
-        MINTER,
-        {'from': DEPLOYER, 'required_confs': confs, 'gas_price': _gas_price()}
-    )
+        abi = next(i['inputs'] for i in zap_deployer.abi if i['type'] == "constructor")
+        args = {
+            '_coins': wrapped_coins,
+            '_underlying_coins': underlying_coins,
+            '_token': token,
+            '_pool': swap,
+            '_curve': swap,
+        }
+        deployment_args = [args[i['name']] for i in abi] + [_tx_params()]
+
+        zap_deployer.deploy(*deployment_args)
