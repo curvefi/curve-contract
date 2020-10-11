e602fa39af363ecc2582d2f8cd3705756b6cb916
Ben Hauser <ben@hauser.id>
2020-10-11 21:51:15 +0300

    test: add support for RSV minting

--- tests/fixtures/coins.py
+++ tests/fixtures/coins.py
@@ -71,11 +71,16 @@ class _MintableTestToken(Contract):
 
     def _mint_for_testing(self, target, amount, tx=None):
         if self.address == "0x674C6Ad92Fd080e4004b2312b45f796a192D27a0":
-            # special case for minting USDN
+            # USDN
             self.deposit(target, amount, {'from': "0x90f85042533F11b362769ea9beE20334584Dcd7D"})
             return
         if self.address == "0x0E2EC54fC0B509F445631Bf4b91AB8168230C752":
+            # LinkUSD
             self.mint(target, amount, {'from': "0x62F31E08e279f3091d9755a09914DF97554eAe0b"})
+            return
+        if self.address == "0x196f4727526eA7FB1e17b2071B3d8eAA38486988":
+            self.changeMaxSupply(2**128, {'from': self.owner()})
+            self.mint(target, amount, {'from': self.minter()})
             return
 
         for address in _holders[self.address].copy():
