906a1cc1af7729ff18b898cea9c74a4c2157f268
samwerner <sammaxwerner@gmail.com>
2021-02-08 23:21:42 +0100

    fix: Resolve conflicts in tests

--- tests/fixtures/coins.py
+++ tests/fixtures/coins.py
@@ -1,12 +1,7 @@
 import pytest
-import requests
-from brownie import ETH_ADDRESS, ZERO_ADDRESS, Contract, ERC20Mock, ERC20MockNoReturn
-from brownie.convert import to_address
-
+from brownie import ETH_ADDRESS, ZERO_ADDRESS, ERC20Mock, ERC20MockNoReturn
+from brownie_tokens import MintableForkToken
 from conftest import WRAPPED_COIN_METHODS
-
-_holders = {}
-
 
 # public fixtures - these can be used when testing
 
@@ -49,7 +44,7 @@ def base_pool_token(project, charlie, base_pool_data, is_forked):
 # private API below
 
 
-class _MintableTestToken(Contract):
+class _MintableTestToken(MintableForkToken):
     def __init__(self, address, pool_data=None):
         super().__init__(address)
 
@@ -59,67 +54,6 @@ class _MintableTestToken(Contract):
             for target, attr in fn_names.items():
                 if hasattr(self, attr) and target != attr:
                     setattr(self, target, getattr(self, attr))
-
-        # get top token holder addresses
-        address = self.address
-        if address not in _holders:
-            holders = requests.get(
-                f"https://api.ethplorer.io/getTopTokenHolders/{address}",
-                params={"apiKey": "freekey", "limit": 50},
-            ).json()
-            _holders[address] = [to_address(i["address"]) for i in holders["holders"]]
-
-    def _mint_for_testing(self, target, amount, tx=None):
-        if self.address == "0x674C6Ad92Fd080e4004b2312b45f796a192D27a0":
-            # USDN
-            self.deposit(target, amount, {"from": "0x90f85042533F11b362769ea9beE20334584Dcd7D"})
-            return
-        if self.address == "0x0E2EC54fC0B509F445631Bf4b91AB8168230C752":
-            # LinkUSD
-            self.mint(target, amount, {"from": "0x62F31E08e279f3091d9755a09914DF97554eAe0b"})
-            return
-        if self.address == "0x196f4727526eA7FB1e17b2071B3d8eAA38486988":
-            # RSV
-            self.changeMaxSupply(2 ** 128, {"from": self.owner()})
-            self.mint(target, amount, {"from": self.minter()})
-            return
-        if self.address == "0x5228a22e72ccC52d415EcFd199F99D0665E7733b":
-            # pBTC
-            self.mint(target, amount, {"from": self.pNetwork()})
-            return
-        if self.name().startswith("Aave"):
-            underlying = _MintableTestToken(self.UNDERLYING_ASSET_ADDRESS())
-            lending_pool = Contract("0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9")
-            underlying._mint_for_testing(target, amount)
-            underlying.approve(lending_pool, amount, {"from": target})
-            lending_pool.deposit(underlying, amount, target, 0, {"from": target})
-            return
-        if self.address == "0x4A64515E5E1d1073e83f30cB97BEd20400b66E10":
-            # wZEC
-            self.mint(target, amount, {"from": "0x5Ca1262e25A5Fb6CA8d74850Da2753f0c896e16c"})
-            return
-        if self.address == "0x1C5db575E2Ff833E46a2E9864C22F4B22E0B37C2":
-            # renZEC
-            self.mint(target, amount, {"from": "0xc3BbD5aDb611dd74eCa6123F05B18acc886e122D"})
-            return
-
-        for address in _holders[self.address].copy():
-            if address == self.address:
-                # don't claim from the treasury - that could cause wierdness
-                continue
-            balance = self.balanceOf(address)
-            try:
-                if amount > balance:
-                    self.transfer(target, balance, {"from": address})
-                    amount -= balance
-                else:
-                    self.transfer(target, amount, {"from": address})
-                    return
-            except Exception:
-                # sometimes tokens just don't want to be stolen
-                pass
-
-        raise ValueError(f"Insufficient tokens available to mint {self.name()}")
 
 
 def _deploy_wrapped(project, alice, pool_data, idx, underlying, aave_lending_pool):
