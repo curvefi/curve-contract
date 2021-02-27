97cf34c09f29cbe0226964ad70e15d45f6e8ee9c
samwerner <sammaxwerner@gmail.com>
2021-02-27 16:35:39 +0100

    tests: Add exchange underlying test with different rates

--- tests/pools/common/unitary/test_exchange_underlying.py
+++ tests/pools/common/unitary/test_exchange_underlying.py
@@ -1,6 +1,8 @@
 from itertools import permutations
 
 import pytest
+from brownie.test import given, strategy
+from hypothesis import settings
 from pytest import approx
 
 pytestmark = [pytest.mark.usefixtures("add_initial_liquidity", "approve_bob"), pytest.mark.lending]
@@ -82,3 +84,34 @@ def test_min_dy_underlying(bob, swap, underlying_coins, sending, receiving, unde
     swap.exchange_underlying(sending, receiving, amount, min_dy - 1, {"from": bob})
 
     assert abs(underlying_coins[receiving].balanceOf(bob) - min_dy) <= 1
+
+
+@given(delta=strategy("decimal", min_value="0.001", max_value=2, places=3))
+@settings(max_examples=10)
+@pytest.mark.itercoins("sending", "receiving", underlying=True)
+def test_exchange_with_rate(
+    bob,
+    swap,
+    underlying_coins,
+    wrapped_coins,
+    sending,
+    receiving,
+    underlying_decimals,
+    base_amount,
+    n_coins,
+    delta,
+):
+    amount = (base_amount // 100) * 10 ** underlying_decimals[sending]
+    expected_dy = swap.get_dy_underlying(sending, receiving, amount)
+    rate = wrapped_coins[receiving].exchangeRateCurrent.call()
+    wrapped_coins[receiving].set_exchange_rate(rate * delta)
+    underlying_coins[sending]._mint_for_testing(bob, amount, {"from": bob})
+
+    tx = swap.exchange_underlying(sending, receiving, amount, 0, {"from": bob})
+    dy = tx.events["TokenExchangeUnderlying"]["tokens_bought"]
+    if delta > 1:
+        assert expected_dy < dy
+    elif delta < 1:
+        assert expected_dy > dy
+    else:
+        assert expected_dy == dy
